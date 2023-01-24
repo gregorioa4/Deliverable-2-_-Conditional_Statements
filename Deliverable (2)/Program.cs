437bdf2d2fe3182e5475059f385ae4d0174d6646
antsg <antsg@ANTHONYS-DESKTO>
2023-01-24 17:34:31 -0500

    Add project files.

---
+++ Deliverable (2)/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Author: Anthony Gregorio
+Date: 1/23/2023
+Description: C# Console Application for calculating letter grades
+*/
+
+using System;
+using System.Diagnostics.Metrics;
+
+
+namespace Deliverable__2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numeric grade: ");
+
+            try
+            {
+                int grade = int.Parse(Console.ReadLine());
+
+                string letter;
+
+                if (grade >= 90)
+                {
+                    letter = "A";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+
+                }
+
+                else if (grade >= 80 && grade < 90)
+                {
+                    letter = "B";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+
+                }
+
+                else if (grade >= 70 && grade < 80)
+                {
+                    letter = "C";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+
+
+                }
+
+                else if (grade >= 60 && grade < 70)
+                {
+                    letter = "D";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+
+
+                }
+
+                else if (grade >= 50 && grade < 60)
+                {
+                    letter = "F";
+                    Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter a numeric value");
+            }
+
+           
+        }
+    }
+}
